c6f0343d3e7162be2c360e793f68534694e07557
ReeceLangerock <ReeceLangerock@gmail.com>
2019-02-26 19:41:42 -0600

    got it working, ready to move to portfolio site

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,48 +1,82 @@
 // src/lambda/graphql.js
-const { ApolloServer, gql } = require("apollo-server-lambda");
+const { ApolloServer, gql } = require('apollo-server-lambda');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const Posts = [
-  {
-    id: 1,
-    title: "test",
-    content: "blah"
-  },
-  {
-    id: 2,
-    title: "test2",
-    content: "blah blah"
-  }
-];
+const MONGO_URI = process.env.MONGO_URI;
+console.log(process.env.MONGO_URI);
+if (!MONGO_URI) {
+	throw new Error('You must provide a MongoLab URI');
+}
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+mongoose.connection
+	.once('open', () => console.log('Connected to MongoLab instance.'))
+	.on('error', error => console.log('Error connecting to MongoLab:', error));
+
+const postSchema = new Schema(
+	{
+		title: String,
+		content: String
+	},
+	{
+		autoIndex: true,
+		timestamps: true
+	}
+);
+
+const Post = mongoose.model('post', postSchema);
 
 const typeDefs = gql`
-  type Query {
-    getPosts: [Post]
-    getPostById: [Post]
-  }
-  type Post {
-    title: String
-    id: Int
-    content: String
-    date: String
-  }
+	type Query {
+		getPosts: [Post]
+		getPostByTitle(title: String!): Post
+		getPostById(id: ID!): Post
+	}
+	type Mutation {
+		addPost(title: String!, content: String!): Post
+	}
+	type Post {
+		id: ID!
+		title: String
+		content: String
+	}
 `;
 
 const resolvers = {
-  Query: {
-    getPosts: (root, args, context) => {
-      return Posts;
-    },
-    getPostById: (parent, args, context, info) => {
-        console.log(args, Posts)
-      return Posts.filter(post => id === args.id);
-    }
-  }
+	Query: {
+		getPosts: async () =>
+			await Post.find({})
+				.limit(2)
+				.exec(),
+
+		getPostByTitle: async (parent, args, context) =>
+			await Post.findOne({ title: args.title }).exec(),
+		getPostById: async (parent, args, context) => await Post.findById({ _id: args.id }).exec()
+	},
+	Mutation: {
+		addPost: async (parent, args, context) => {
+			try {
+				let response = await Post.create(args);
+				return response;
+			} catch (e) {
+				return e.message;
+			}
+		}
+	}
 };
+
 const server = new ApolloServer({
-  introspection: true,
-  playground: true,
-  typeDefs,
-  resolvers
+	introspection: true,
+	playground: true,
+	typeDefs,
+	resolvers,
+	context: ({ req }) => ({
+		Post
+	})
 });
 
 exports.handler = server.createHandler();
